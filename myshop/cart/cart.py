7a5e9e5b7cedf60c6e2896794efe014273988f6c
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-01-15 11:34:14 +0200

    added cart

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -11,7 +11,7 @@ class Cart(object):
         self.cart = cart
     
     def add(self, product, quantity = 1, update_quantity = False):
-        product_id = str(product_id)
+        product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] ={'quantity':0, "price":str(product.price)}
         if update_quantity:
@@ -20,7 +20,7 @@ class Cart(object):
             self.cart[product_id]['quantity'] += quantity
         self.save()
     
-    def sef save(self):
+    def save(self):
         self.session.modified =True
     
     def remove(self, product):
@@ -34,14 +34,14 @@ class Cart(object):
             Decimal(item['price']) * item['quantity']
             for item in self.cart.values()
         )
-        
+
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
 
     def __iter__(self):
         product_ids = self.cart.keys()
-        products = Product.object.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
 
         cart = self.cart.copy()
         for product in products:
@@ -49,10 +49,10 @@ class Cart(object):
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price']=item['price']*item['quantity']
-        yield item
+            yield item
 
     def __len__(self):
-        reurn sum(item['quantity'] for item in self.cart.values())
+        return sum(item['quantity'] for item in self.cart.values())
 
 
     
